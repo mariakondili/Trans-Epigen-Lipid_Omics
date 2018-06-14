489eff4b51bf11987064d0d7d7637e8a55c868ac
mariakondili <mariakondili13@gmail.com>
2018-06-14 11:13:35 +0200

    chng name,fix comments

--- run_MUSIC.sh
+++ run_MUSIC.sh
@@ -1,99 +1,50 @@
 #!/bin/bash
 
+## Author : Maria Kondili
+
+
 ### Apply MUSIC for peak detection per replicate
 ### Tutorial  : https://github.com/gersteinlab/MUSIC
 
-## Executable in :'~/MyTools/MUSIC-master/bin/' --> exported $PATH
 
-### Run MUSIC for Enriched Regions detection on IRF5-trimmed-q10-rmDup reads : 
-homedir="/media/kondmar/my2TB/PROJECTS/NV/IRF5_ChIPseq/bam/BAM_Trim_q10_RmDup/"
-output_dir="/media/kondmar/my2TB/PROJECTS/NV/IRF5_ChIPseq/MUSIC_PeakCalling/"
+### Run MUSIC for Enriched Regions detection on Trimmed/mapped_qual=10/rmDup reads :
 
-cd $homedir
-WT=(ls WT/*.bam)
-WT30='WT/SE/WT_030m_R1_sorted_rmDup.bam'
-KO=(ls KO/*.bam)
-input_30='Input_SE/IRF5_Input_SE_sorted_q10_rmDup.bam'
+homedir="/home/kondmar/PROJECTS/ChIPseq/bam/Trimmed/Filtered/" 
+output_dir="/home/kondmar/PROJECTS/ChIPseq/MUSIC_PeakCalling/"
+
+WT=(ls  $homedir'WT/*.bam')
+KO=(ls  $homedir'KO/*.bam')  ## Input can be used equally
+
 
 ## PRE-PROCESS 
-mkdir $output_dir'chip_0m_R1'
-samtools view ${WT[1]} | MUSIC -preprocess SAM stdin $output_dir'chip_0m_R1'
-mkdir $output_dir'chip_0m_R2'
-samtools view ${WT[2]} | MUSIC -preprocess SAM stdin $output_dir'chip_0m_R2'
 
-samtools view ${WT[3]} | MUSIC -preprocess SAM stdin $output_dir'chip_120m_R1'
-samtools view ${WT[4]} | MUSIC -preprocess SAM stdin $output_dir'chip_120m_R2'
+for i in ${!WT[@]}; do 
+	wt=${WT[i]}
+	ko=${KO[i]}
+	name_wt=${wt:4:8}
+	name_ko=${ko:4:8}
+	
+	mkdir $output_dir$name
+	samtools view $wt | MUSIC -preprocess SAM stdin $output_dir$name_wt
+	samtools view $ko | MUSIC -preprocess SAM stdin $output_dir$name_ko
 
-mkdir $output_dir'chip_30m'
-samtools view $WT30  | MUSIC -preprocess SAM stdin $output_dir'chip_30m'
-samtools view $input_30  | MUSIC -preprocess SAM stdin $output_dir'input_30m'
+	mkdir $output_dir'sorted/'$name 
+	mkdir $output_dir'dedup/'$name
 
-samtools view ${KO[1]} | MUSIC -preprocess SAM stdin $output_dir'KO_0m_R1'
-samtools view ${KO[2]} | MUSIC -preprocess SAM stdin $output_dir'KO_0m_R2'
-samtools view ${KO[3]} | MUSIC -preprocess SAM stdin $output_dir'KO_120m_R1'
-samtools view ${KO[4]} | MUSIC -preprocess SAM stdin $output_dir'KO_120m_R2'
+	MUSIC -sort_reads $output_dir$name_wt  $output_dir'sorted/'$name_wt
 
-mkdir sorted/chip_0m;mkdir sorted/chip_120m; mkdir sorted/chip_30m
-mkdir sorted/KO_0m; mkdir sorted/KO_120m;mkdir sorted/input_30m
+	MUSIC -remove_duplicates  $output_dir'sorted/'$name_wt 2  $output_dir'dedup/'$name_wt
+	MUSIC -remove_duplicates  $output_dir'sorted/'$name_ko 2  $output_dir'dedup/'$name_ko
 
-MUSIC -sort_reads $output_dir'chip_0m_R1'   $output_dir'sorted/chip_0m_R1'
-MUSIC -sort_reads $output_dir'chip_0m_R2'   $output_dir'sorted/chip_0m_R2'
-#120m
-MUSIC -sort_reads $output_dir'chip_120m_R1' $output_dir'sorted/chip_120m_R1'
-MUSIC -sort_reads $output_dir'chip_120m_R2' $output_dir'sorted/chip_120m_R2'
-
-MUSIC -sort_reads $output_dir'chip_30m' $output_dir'sorted/chip_30m'
-MUSIC -sort_reads $output_dir'input_30m' $output_dir'sorted/Input_30m'
-
-MUSIC -sort_reads $output_dir'KO_0m_R1' $output_dir'sorted/KO_0m_R1'
-MUSIC -sort_reads $output_dir'KO_0m_R2' $output_dir'sorted/KO_0m_R2'
-
-MUSIC -sort_reads $output_dir'KO_120m_R1' $output_dir'sorted/KO_120m_R1'
-MUSIC -sort_reads $output_dir'KO_120m_R2' $output_dir'sorted/KO_120m_R2'
-
-
-MUSIC -remove_duplicates  $output_dir'sorted/chip_0m_R1' 2  $output_dir'dedup/chip_0m_R1'
-MUSIC -remove_duplicates  $output_dir'sorted/chip_0m_R2' 2  $output_dir'dedup/chip_0m_R2'
-
-MUSIC -remove_duplicates  $output_dir'sorted/chip_120m_R1' 2  $output_dir'dedup/chip_120m_R1'
-MUSIC -remove_duplicates  $output_dir'sorted/chip_120m_R2' 2  $output_dir'dedup/chip_120m_R2'
-
-MUSIC -remove_duplicates $output_dir'sorted/chip_30m' 2  $output_dir'dedup/chip_30m'
-MUSIC -remove_duplicates $output_dir'sorted/Input_30m' 2 $output_dir'dedup/Input_30m'
-
-MUSIC -remove_duplicates $output_dir'sorted/KO_0m_R1' 2 $output_dir'dedup/KO_0m_R1'
-MUSIC -remove_duplicates $output_dir'sorted/KO_0m_R2' 2 $output_dir'dedup/KO_0m_R2'
-
-MUSIC -remove_duplicates $output_dir'sorted/KO_120m_R1' 2  $output_dir'dedup/KO_120m_R1'
-MUSIC -remove_duplicates $output_dir'sorted/KO_120m_R2' 2  $output_dir'dedup/KO_120m_R2'
-
-
-cd /media/kondmar/my2TB/PROJECTS/NV/IRF5_ChIPseq/MUSIC_PeakCalling/dedup/
-
-#MUSIC -get_TF_peaks -chip chip -control input -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 75 	-q_val 0.1
-
-mkdir Peaks_chip_0m_R1/
-cd Peaks_chip_0m_R1/
-
-MUSIC -get_TF_peaks -chip $output_dir'dedup/chip_0m_R1' -control  $output_dir'dedup/KO_0m_R1' -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1 
-MUSIC -get_TF_peaks -chip $output_dir'dedup/chip_0m_R2' -control  $output_dir'dedup/KO_0m_R2' -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1
-
-mkdir Peaks_chip_120m_R1, _R2
-
-MUSIC -get_TF_peaks -chip $output_dir'dedup/chip_120m_R1' -control  $output_dir'dedup/KO_120m_R1' -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1
-MUSIC -get_TF_peaks -chip $output_dir'dedup/chip_120m_R2' -control  $output_dir'dedup/KO_120m_R2' -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1
-
-mkdir Peaks_chip_30m
-cd Peaks_chip_30m
-MUSIC -get_TF_peaks -chip $output_dir'dedup/chip_30m' -control  $output_dir'dedup/Input_30m' -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1
-
+	mkdir 'Peaks_chip/'$name
+	MUSIC -get_TF_peaks -chip $output_dir'dedup/'$name_wt -control  $output_dir'dedup/'$name_ko -begin_l 1000 -end_l 16000 -step 1.5 -l_frag 300 -q_val 0.1 
+done 
 
 ## Concatenate all chromosomes per Sample : 
 mrg_peaks=$output_dir"Merged_Peaks/"
 mkdir $output_dir"Merged_Peaks/"
-cat Peaks_chip_0m_R1/*.bed >  All_SSERS_WT0m_R1.bed
-cat Peaks_chip_120m_R1/*.bed > All_SSERS_WT30m.bed
-cat Peaks_chip_30m/*.bed >  All_SSERS_WT120m_R1.bed
+
+cat 'Peaks_'$name'/*.bed' > 'All_SSERS'$name'.bed'
 
 mrg_0_R1=$mrg_peaks'All_SSERS_WT0m_R1.bed'
 mrg_0_R2=$mrg_peaks'All_SSERS_WT0m_R2.bed'
@@ -101,9 +52,11 @@ mrg_120_R1=$mrg_peaks'All_SSERS_WT120m_R1.bed'
 mrg_120_R2=$mrg_peaks'All_SSERS_WT120m_R2.bed'
 
 ### INTERSECT R1+R2 peaks( unique one in one Replicate or overlaps ) 
-intersectBed -wa -wb -a $mrg_0_R1 -b $mrg_0_R2 | awk 'BEGIN {OFS="\t"} {if ($2 < $11) start=$2; else start=$11} {if ($3 > $12) end=$3; else end=$12} {print  $1,start,end,($5+$14)/2, $6,$7, $8, $9}' -  > $mrg_peaks"WT_0m_R1_ovl_R2.bed"
 
-intersectBed -wa -wb -a $mrg_120_R1 -b $mrg_120_R2 | awk 'BEGIN {OFS="\t"} {if ($2 < $11) start=$2; else start=$11} {if ($3 > $12) end=$3; else end=$12} {print  $1,start,end,($5+$14)/2, $6,$7, $8, $9}' -  > $mrg_peaks"WT_120m_R1_ovl_R2.bed"
+intersectBed -wa -wb -a $mrg_0_R1 -b $mrg_0_R2 | awk 'BEGIN {OFS="\t"} {if ($2 < $11) start=$2; else start=$11} {if ($3 > $12) end=$3; else end=$12} {print  $1,start,end,($5+$14)/2}' -  > $mrg_peaks"WT_0m_R1_ovl_R2.bed"
+
+intersectBed -wa -wb -a $mrg_120_R1 -b $mrg_120_R2 | awk 'BEGIN {OFS="\t"} {if ($2 < $11) start=$2; else start=$11} {if ($3 > $12) end=$3; else end=$12} {print  $1,start,end,($5+$14)/2}' -  > $mrg_peaks"WT_120m_R1_ovl_R2.bed"
+
 
 ## Find which peaks are not included in the overlaps to add them over
 
@@ -113,7 +66,10 @@ bedtools intersect -a  $mrg_peaks'All_SSERS_WT120m_R1.bed' -b $mrg_peaks'WT_120m
 bedtools intersect -a  $mrg_peaks'All_SSERS_WT0m_R2.bed'   -b $mrg_peaks'WT_0m_R1_ovl_R2.bed'   -v > $mrg_peaks'WT_0m_R2_uniq.bed'
 bedtools intersect -a  $mrg_peaks'All_SSERS_WT120m_R2.bed' -b $mrg_peaks'WT_120m_R1_ovl_R2.bed' -v > $mrg_peaks'WT_120m_R2_uniq.bed'
 
+##  Final merging of Unique and overlapping Peaks 
+
 cat $mrg_peaks'WT_0m_R1_uniq.bed' $mrg_peaks'WT_0m_R2_uniq.bed'  $mrg_peaks"WT_0m_R1_ovl_R2.bed"   >  $mrg_peaks'WT_0m_merged+R1R2uniq.bed'
+
 cat $mrg_peaks'WT_120m_R1_uniq.bed' $mrg_peaks'WT_0m_R2_uniq.bed'  $mrg_peaks"WT_120m_R1_ovl_R2.bed" >   $mrg_peaks'WT_120m_merged+R1R2uniq.bed'
 
 
