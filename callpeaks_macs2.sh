4afaa6d05a757b19ce4b8387427e980e007d31a3
mariakondili <mariakondili13@gmail.com>
2018-06-14 11:21:55 +0200

    author,subj added

--- callpeaks_macs2.sh
+++ callpeaks_macs2.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/bash
 
-homedir="/media/kondmar/ChIPseq/bam/Trimmed/Filtered/"
+## Author : Maria Kondili
+
+## Subject : Call Peaks using MACS2 in broad and Narrow mode, inputting my bam files after  trimming adapters and filtering by mapping Quality.
+
+
+
+homedir="/home/kondmar/ChIPseq/bam/Trimmed/Filtered/"
 cd $homedir
 
 samples=(`ls *.bam`) ## ls+ path relative to working dir.
 #inputsdir=(`ls bam/input/*.bam`)
 ctrl=(`ls KO/*.bam`) 
 
-outdir_narrow='/media/kondmar/ChIPseq/bam/Trimmed/Filtered/MACS2_PeakCalling/Narrow/'
-outdir_broad='/media/kondmar/ChIPseq/bam/Trimmed/Filtered/MACS2_PeakCalling/Broad/'
+outdir_narrow=$homedir'MACS2_PeakCalling/Narrow/'
+outdir_broad=$homedir'MACS2_PeakCalling/Broad/'
 
 mkdir $outdir_narrow
 mkdir $outdir_broad
@@ -21,10 +27,11 @@ for i in ${!samples[@]}; do  # ${!dir[@]} : gives the indices of the elem in dir
 	name=${bam:0:10} # capture 1st character and 10 more from bam filename
 
 	## Narrow Peaks 
-	macs2 callpeak -t $bam -c  $ko --format=BAMPE --bdg --qvalue=0.2 --mfold 10 30 --gsize 1.87e9 --name $name --outdir $outdir_narrow$name 2> $outdir_narrow$name".log"
+	macs2 callpeak -t $bam -c  $ko --format=BAMPE --bdg --qvalue=0.1 --mfold 10 30 --gsize 1.87e9 --name $name --outdir $outdir_narrow$name 2> $outdir_narrow$name".log"
 	#OR : Broad Peaks 
-	macs2 callpeak -t $bam -c  $ko --format=BAMPE --bdg --broad --broad-cutoff 0.2 --mfold 10 30 --gsize 1.87e9 --name $name --outdir $outdir_broad$name 2> $outdir_broad$name".log"
+	macs2 callpeak -t $bam -c  $ko --format=BAMPE --bdg --broad --broad-cutoff 0.1 --mfold 10 30 --gsize 1.87e9 --name $name --outdir $outdir_broad$name 2> $outdir_broad$name".log"
 
 done
 
 
+## Thanks to Tariq Khoyratty for suggestions on the flags to use. 
